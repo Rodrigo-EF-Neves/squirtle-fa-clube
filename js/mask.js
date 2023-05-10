008a4a507d82c3b08ff19668a2994f31927f8695
Rodrigo-EF-Neves <rodrigo.emilio2004@gmail.com>
2023-05-10 17:00:14 -0300

    Máscara para CPF sem replace

--- js/mask.js
+++ js/mask.js
@@ -1,6 +1,6 @@
 function aplicaMascaraCpf(cpf){
     cpf = document.querySelector("#txtcpf");
-    cpf.addEventListener('keypress', () => {
+    cpf.addEventListener('keyup', () => {
         let cpfLength = cpf.value.length
         
         if(cpfLength === 3 || cpfLength === 7){
@@ -8,9 +8,14 @@ function aplicaMascaraCpf(cpf){
         } else if (cpfLength === 11){
             cpf.value += "-";
         }
+        // TO DO replace format
     })
     return cpf;
 // replace
 // /^(\d{3})(\d{3})(\d{3})(\d{2})/ 
 // "$1.$2.$3-$4"
+}
+
+function aplicaMascaraTelefone(){
+    
 }
